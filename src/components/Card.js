c9df299921cc5cc964cc257ac27040ccda3910a8
Esin Andrew <kubanesin@gmail.com>
2022-02-26 17:09:05 +0300

    Контекст пользователя, начал использовать в компоненте Card, работа с копкой удаления карточки

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Card(props) {
+
+   // Подписываемся на контекст CurrentUserContext
+   const currentUser = React.useContext(CurrentUserContext);
+
+   // Определяем, являемся ли мы владельцем текущей карточки
+   const isOwn = props.card.owner._id === currentUser._id;
+
+   // Создаём переменную, которую после зададим в className для кнопки удаления
+   const cardDeleteButtonClassName = (
+      `photo__trash ${isOwn ? 'photo__trash_type_active' : ''}`
+   );
 
    function handleClick() {
 
@@ -11,7 +23,7 @@ function Card(props) {
 
       <div className="photo">
          <div className="photo__element" >
-            <button className="photo__trash" type="button" aria-label="Кнопка для удаления "></button>
+            <button className="cardDeleteButtonClassName" type="button" aria-label="Кнопка для удаления "></button>
             <img className="photo__image" onClick={handleClick} src={props.card.link} alt={props.card.name} />
             <div className="photo__title">
                <h2 className="photo__text">{props.card.name}</h2>
